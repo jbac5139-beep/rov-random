1cb88b378a369c585dd1034b5679a61424bbf9de
jbac5139-beep <jbac5139@jbac.ac.th>
2026-07-19 00:13:48 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,46 @@
+// data.js - คลังข้อมูลรูปภาพ ฮีโร่ เลน รูน พลังแฝง และไอเทม RoV
+
+const heroesData = [
+    { name: "Yorn", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/6/60/Yorn_Icon.png" },
+    { name: "Violet", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/f/f6/Violet_Icon.png" },
+    { name: "Raz", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/b/b8/Raz_Icon.png" },
+    { name: "Florentino", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/a/a2/Florentino_Icon.png" },
+    { name: "Nakroth", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/9/93/Nakroth_Icon.png" },
+    { name: "Alice", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/6/60/Alice_Icon.png" },
+    { name: "Maloch", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/1/1e/Maloch_Icon.png" },
+    { name: "Liliana", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/f/f0/Liliana_Icon.png" }
+];
+
+const positionsData = [
+    { name: "Dark Slayer (บน)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/f/f8/Icon_Class_Warrior.png" },
+    { name: "Mid Lane (กลาง)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/0/07/Icon_Class_Mage.png" },
+    { name: "Abyssal Dragon (ล่าง)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/6/60/Icon_Class_Archer.png" },
+    { name: "Jungle (ฟาร์มป่า)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/b/b3/Icon_Class_Assassin.png" },
+    { name: "Support (โรม)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/e/e0/Icon_Class_Support.png" }
+];
+
+const runesRed = ["Awake", "Onslaught", "Rage", "Violate", "Indomitable", "Atrocity"];
+const runesPurple = ["Assassinate", "Guerrilla", "Benevolence", "Bloodlust", "Devour", "Spirit"];
+const runesGreen = ["Dragon's Claw", "Skewer", "Flurry", "Valiance", "Purity", "Focus"];
+
+const enchantmentsData = [
+    { name: "Holy Thunder (Veda)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/e/eb/Enchantment_Holy_Thunder.png" },
+    { name: "Sacred Protection (Veda)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/c/c1/Enchantment_Sacred_Protection.png" },
+    { name: "Desperate Duel (Lokheim)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/d/df/Enchantment_Desperate_Duel.png" },
+    { name: "Curse of Death (Lokheim)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/5/52/Enchantment_Curse_of_Death.png" },
+    { name: "Nature's Gift (Afata)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/0/07/Enchantment_Nature%27s_Gift.png" },
+    { name: "Visceral Boost (League)", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/2/23/Enchantment_Visceral_Boost.png" }
+];
+
+const itemsPoolData = [
+    { name: "War Boots", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/0/0e/War_Boots_Icon.png" },
+    { name: "Gilded Greaves", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/f/f6/Gilded_Greaves_Icon.png" },
+    { name: "Claves Sancti", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/d/d4/Claves_Sancti_Icon.png" },
+    { name: "Fenrir's Tooth", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/0/07/Fenrir%27s_Tooth_Icon.png" },
+    { name: "Muramasa", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/f/f9/Muramasa_Icon.png" },
+    { name: "Boomstick", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/7/7e/Boomstick_Icon.png" },
+    { name: "Hecate's Diadem", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/6/60/Hecate%27s_Diadem_Icon.png" },
+    { name: "The Aegis", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/e/ea/The_Aegis_Icon.png" },
+    { name: "Gaia's Standard", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/e/ef/Gaia%27s_Standard_Icon.png" },
+    { name: "Blade of Eternity", imgUrl: "https://static.wikia.nocookie.net/arena-of-valor/images/b/b3/Blade_of_Eternity_Icon.png" }
+];
